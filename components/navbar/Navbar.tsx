f8f8a9ffc384f8790133e16c4d7f5a971f6b769e
Afnan Sohail <afnan.sohail@emumba.com>
2025-02-07 15:08:57 +0500

    created navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,16 @@
+import { HomeIcon } from "@/public/icons";
 import styles from "./Navbar.module.css";
+
 const Navbar = () => {
-  return <div className={styles.navbar}>Navbar</div>;
+  return (
+    <div className={styles.navbar}>
+      <HomeIcon />
+      <div className={styles.navbarContent}>
+        <h1>NPM Package Comparator</h1>
+        <p>Compare & Recommend the best NPM package</p>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
